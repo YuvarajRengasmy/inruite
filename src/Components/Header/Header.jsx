67152fadf7f29a943dbcb7975f87b02ec41bbab7
balan081194 <balancivil9133@gmail.com>
2024-06-13 14:32:43 +0530

    about

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const Header = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-white ">
+      <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
           <a className="navbar-brand " href="#">
             <img
@@ -25,13 +25,13 @@ export const Header = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse  justify-content-end mx-5 " id="navbarNav" >
             <ul className="navbar-nav ms-auto  mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/About">About</a>
+                <a className="nav-link" href="/About">About us</a>
               </li>
               <li className="nav-item dropdown">
                 <a
@@ -44,9 +44,22 @@ export const Header = () => {
                   Industries
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Home</a></li>
-                  <li><a className="dropdown-item" href="#">About</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li><a className="dropdown-item" href="#">Civil & Construction</a></li>
+                  <li><a className="dropdown-item" href="#">Engineering</a></li>
+                  <li><a className="dropdown-item" href="#">Hospitality</a></li>
+                  <li><a className="dropdown-item" href="#">Energy & Resources</a></li>
+                  <li><a className="dropdown-item" href="#">Manufacturing</a></li>
+                  <li><a className="dropdown-item" href="#">IT & Telecom</a></li>
+                  <li><a className="dropdown-item" href="#">Oil & Gas</a></li>
+                  <li><a className="dropdown-item" href="#">HealthCare</a></li>
+                  <li><a className="dropdown-item" href="#">Travel & Tourism</a></li>
+                  <li><a className="dropdown-item" href="#">FCMG</a></li>
+                  <li><a className="dropdown-item" href="#">Marine / Shipping</a></li>
+                  <li><a className="dropdown-item" href="#">Education</a></li>
+                  <li><a className="dropdown-item" href="#">Distribution & Logistics</a></li>
+                  <li><a className="dropdown-item" href="#">Agriculture</a></li>
+                  <li><a className="dropdown-item" href="#">Automobile</a></li>
+                  <li><a className="dropdown-item" href="#">Accounting & Operations</a></li>
                 </ul>
               </li>
               <li className="nav-item dropdown">
@@ -60,9 +73,15 @@ export const Header = () => {
                   Services
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Home</a></li>
-                  <li><a className="dropdown-item" href="#">About</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li><a className="dropdown-item" href="#">Overseas Recruitment</a></li>
+                  <li><a className="dropdown-item" href="#">Executive Search</a></li>
+                  <li><a className="dropdown-item" href="#">Contract Hiring</a></li>
+                  <li><a className="dropdown-item" href="#">Permanent Staffing</a></li>
+                  <li><a className="dropdown-item" href="#">PayRoll Management</a></li>
+                  <li><a className="dropdown-item" href="#">Job Fair</a></li>
+                  <li><a className="dropdown-item" href="#">Campus Recruitment</a></li>
+                  <li><a className="dropdown-item" href="#">Technical Trade Test</a></li>
+                  <li><a className="dropdown-item" href="#">Ticketing and Emigration</a></li>
                 </ul>
               </li>
               <li className="nav-item">
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export const About = () => {
   return (
-    <div>About</div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-tertiary shadow p-3 mb-5 bg-body-tertiary rounded">
+        <Link className="navbar-brand" to="/"> <img
+          src="https://www.inrecruit.in/images/logo/fynd-career.png"
+          alt="InRecurit Logo"
+          className='img-fluid '
+          style={{ maxWidth: '200px' }}
+        /></Link>
+
+
+        <div className="collapse navbar-collapse  justify-content-end mx-5 " id="navbarNav" >
+          <ul className="navbar-nav mx-2 "    >
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" style={{ color: "#0f2239" }} to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" style={{ color: "#0f2239" }} to="/about">About us</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/industries" style={{ color: "#0f2239" }}>Indusries</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/services" style={{ color: "#0f2239" }}>Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/process" style={{ color: "#0f2239" }}>Process</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/client" style={{ color: "#0f2239" }}>Clients</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/carrers" style={{ color: "#0f2239" }}>Carrers</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/blogs" style={{ color: "#0f2239" }}>Blogs</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link mx-1 fw-bold" to="/contact" style={{ color: "#0f2239" }}>Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className='container-fluid'>
+        <div class="row">
+          <div class="col-md-12">
+          <img src="https://media.istockphoto.com/id/1434341455/photo/diversity-business-team-and-fist-bump-in-office-with-support-team-building-and-employee.jpg?s=1024x1024&w=is&k=20&c=gpIIJicSJh2x3BV5j29VYDW3frv1hTR7akntMT47yRQ=" className="img-fluid w-100"
+          style={{height: '500px' }} alt="..." />
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 export default About
